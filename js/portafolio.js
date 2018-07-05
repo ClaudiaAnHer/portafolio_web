ec3d241f61d939db5b72a429bba7d944a617953c
ClaudiaAnHer <claudia.aninir@gmail.com>
2018-07-05 02:46:28 -0400

    primer

---
+++ js/portafolio.js
@@ -0,0 +1,35 @@
+$(".1").hover(function(){
+    $("#web").show();
+}, function(){
+    $("#web").hide();
+});
+
+$(".2").hover(function(){
+    $("#branding").show();
+}, function(){
+    $("#branding").hide();
+});
+
+$(".3").hover(function(){
+    $("#ilustracion").show();
+}, function(){
+    $("#ilustracion").hide();
+});
+
+$(".4").hover(function(){
+    $("#afiches").show();
+}, function(){
+    $("#afiches").hide();
+});
+
+$(".5").hover(function(){
+    $("#editorial").show();
+}, function(){
+    $("#editorial").hide();
+});
+
+$(".6").hover(function(){
+    $("#contacto").show();
+}, function(){
+    $("#contacto").hide();
+});
